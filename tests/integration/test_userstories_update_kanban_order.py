ba64faae24bdb26ba490a04f2860ed3e715eac6d
code-review-doctor <contact+django-doctor-test@richardtier.co.uk>
2022-02-21 09:53:19 +0000

    Fix issue duplicate-test-names found at https://codereview.doctor

--- tests/integration/test_userstories_update_kanban_order.py
+++ tests/integration/test_userstories_update_kanban_order.py
@@ -737,7 +737,7 @@ def test_api_update_orders_in_bulk_invalid_affter_us_because_no_swimlane(client)
     assert "after_userstory_id" in response.data
 
 
-def test_api_update_orders_in_bulk_invalid_affter_us_because_project(client):
+def test_api_update_orders_in_bulk_invalid_affter_us_because_project_two(client):
     project = f.create_project()
     f.MembershipFactory.create(project=project, user=project.owner, is_admin=True)
     status = f.UserStoryStatusFactory.create(project=project)
@@ -767,7 +767,7 @@ def test_api_update_orders_in_bulk_invalid_affter_us_because_project(client):
     assert "before_userstory_id" in response.data
 
 
-def test_api_update_orders_in_bulk_invalid_affter_us_because_status(client):
+def test_api_update_orders_in_bulk_invalid_affter_us_because_status_two(client):
     project = f.create_project()
     f.MembershipFactory.create(project=project, user=project.owner, is_admin=True)
     status = f.UserStoryStatusFactory.create(project=project)
@@ -798,7 +798,7 @@ def test_api_update_orders_in_bulk_invalid_affter_us_because_status(client):
     assert "before_userstory_id" in response.data
 
 
-def test_api_update_orders_in_bulk_invalid_affter_us_because_swimlane(client):
+def test_api_update_orders_in_bulk_invalid_affter_us_because_swimlane_two(client):
     project = f.create_project()
     f.MembershipFactory.create(project=project, user=project.owner, is_admin=True)
     status = f.UserStoryStatusFactory.create(project=project)
@@ -829,7 +829,7 @@ def test_api_update_orders_in_bulk_invalid_affter_us_because_swimlane(client):
     assert "before_userstory_id" in response.data
 
 
-def test_api_update_orders_in_bulk_invalid_affter_us_because_no_swimlane(client):
+def test_api_update_orders_in_bulk_invalid_affter_us_because_no_swimlane_two(client):
     project = f.create_project()
     f.MembershipFactory.create(project=project, user=project.owner, is_admin=True)
     status = f.UserStoryStatusFactory.create(project=project)
